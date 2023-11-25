6a41a5901a964908a5ff045ed7149ea04ff90e4e
zaanetaa <zanetaszustakiewicz@outlook.com>
2023-11-25 18:54:13 +0100

    configuration to return empty list []

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-# Nie restartuje przy zmianach:
-# flask --app main run
-
-# Restart przy zmianie
-# flask --app main --debug run
-from flask import Flask, render_template
+from flask import Flask
+from products.main import products
 
 app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
-
-@app.route('/hello/<name>', methods=["GET"])
-def say_hello(name):
-    return render_template('say_hello.html', name=name)
+app.register_blueprint(products, url_prefix="/products")
